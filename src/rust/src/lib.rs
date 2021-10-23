83ea5bc983be13058b4f74af38042425d9920d8e
Hiroaki Yutani <yutani.ini@gmail.com>
2021-10-23 12:06:20 +0900

    Update

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -1,6 +1,8 @@
 use extendr_api::prelude::*;
 use polars::prelude::{Int32Chunked, NewChunkedArray, TakeRandom};
 
+// TODO: Debug should be implemented by ourselves to make the result of
+// `.Internal(inspect(x))` nicer.
 #[derive(Debug, Clone)]
 struct AltInt32Chunked(Int32Chunked);
 
@@ -15,16 +17,27 @@ impl AltIntegerImpl for AltInt32Chunked {
         if let Some(i) = self.0.get(index) {
             i
         } else {
-            // TODO: can we return NA?
-            unimplemented!()
+            // TODO: can we return NA more nicely?
+            Rint::na().inner()
         }
     }
 }
 
 /// @export
 #[extendr(use_try_from = true)]
-fn as_in32chunked(x: &[i32]) -> Robj {
-    let x_chunked = Int32Chunked::new_from_slice("slice", x);
+fn as_in32chunked(x: Integers) -> Robj {
+    let x_chunked = Int32Chunked::new_from_iter(
+        "int32_chunked",
+        x.iter().map(|i| {
+            if i.is_na() {
+                // TODO: handle NA properly
+                unimplemented!()
+            } else {
+                i.inner()
+            }
+        }),
+    );
+
     let state = AltInt32Chunked(x_chunked);
     let class = Altrep::make_altinteger_class::<AltInt32Chunked>("int32_chunked", "polarsr");
     Altrep::from_state_and_class(state, class, false).into()
